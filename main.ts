cf1260e660c28c6ca19815556ac55db26f79b224
Austin Lian <105499360+LianAustin@users.noreply.github.com>
2023-03-29 10:55:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
 hummingbird.startHummingbird()
 basic.forever(function () {
-    hummingbird.setLED(ThreePort.One, 0)
+    hummingbird.setLED(ThreePort.One, 70)
     basic.pause(500)
     hummingbird.setLED(ThreePort.One, 0)
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.One,
+    100,
+    0,
+    0
+    )
+    basic.pause(500)
+})
+basic.forever(function () {
+    hummingbird.setTriLED(
+    TwoPort.One,
+    0,
+    100,
+    0
+    )
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.One,
+    0,
+    0,
+    100
+    )
     basic.pause(500)
 })
